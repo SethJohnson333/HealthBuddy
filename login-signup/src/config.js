a0449537c0b3e062695d6f0df19b2309bd912dd5
ChiragSeth14 <135326086+ChiragSeth14@users.noreply.github.com>
2024-10-12 23:19:35 -0400

    added more fields to the table

--- login-signup/src/config.js
+++ login-signup/src/config.js
@@ -2,26 +2,46 @@ const mongoose = require('mongoose');
 const connect = mongoose.connect("mongodb://localhost:27017/login-signup");
 
 connect.then(() => {
-    console.log("Database Connected Successfully");
+   console.log("Database Connected Successfully");
 })
 .catch(() => {
-    console.log("Database cannot be conncected");
+   console.log("Database cannot be conncected");
 });
 
 const LoginSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true,
-        enum: ['patient', 'doctor']
-    }
+   name: {
+       type: String,
+       required: true
+   },
+   password: {
+       type: String,
+       required: true
+   },
+   role: {
+       type: String,
+       required: true,
+       enum: ['patient', 'doctor']
+   },
+   transcripts: {
+       type: [String],
+       default: []
+   },
+   summarizedDiagnosis: {
+       type: String,
+       default: ''
+   },
+   treatmentSteps: {
+       type: [String],
+       default: []
+   },
+   appointmentSchedule: {
+       type: String,
+       default: ''
+   },
+   prescribedMedication: {
+       type: [String],
+       default: []
+   }
 })
 
 const collection = new mongoose.model("users", LoginSchema);
